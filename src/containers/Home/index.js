2c7614438c0e1c3c13bd3749da3c14d6061eaf4e
Luckey <luckeysherpa07@gmail.com>
2020-12-25 12:20:11 +0545

    added list to home screen

--- src/containers/Home/index.js
+++ src/containers/Home/index.js
@@ -1,15 +1,46 @@
 import React from "react";
-import { 
+import {
   View,
   Text,
-  StyleSheet
+  StyleSheet,
+  FlatList
 } from "react-native";
+import { ListItem, Avatar } from 'react-native-elements'
+
+const list = [
+  {
+    room: '1',
+    name: 'Amy Farha',
+    subtitle: 'Vice President'
+  },
+  {
+    room: '2',
+    name: 'Chris Jackson',
+    avatar_url: 'https://s3.amazonaws.com/uifaces/faces/twitter/adhamdannaway/128.jpg',
+    subtitle: 'Vice Chairman'
+  },
+]
+
+keyExtractor = (item, index) => index.toString()
+
+renderItem = ({ item }) => (
+  <ListItem bottomDivider >
+    <Avatar title={item.name[0]} source={item.avatar_url && { uri: item.avatar_url }}/>
+    <ListItem.Content>
+      <ListItem.Title>{item.name}</ListItem.Title>
+      <ListItem.Subtitle>Room No: {item.room}</ListItem.Subtitle>
+    </ListItem.Content>
+    <ListItem.Chevron />
+  </ListItem>
+)
 
 const Home = (props) => (
-  <View style={styles.container}>
-    <Text>Home</Text>
-  </View>
-  )
+  <FlatList
+    keyExtractor={this.keyExtractor}
+    data={list}
+    renderItem={this.renderItem}
+  />
+)
 export default Home;
 
 const styles = StyleSheet.create({
